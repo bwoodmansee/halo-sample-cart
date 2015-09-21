3322224d253d31e898971bc84f762acecdffb1cb
Brandon Woodmansee <brandon.woodmansee@hautelook.com>
2015-09-21 16:54:41 -0700

    BS Submission

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -27,6 +27,11 @@ class FeatureContext extends BehatContext
     {
     }
 
+// OK
+// Scenario: Empty cart subtotal
+//   Given I have an empty cart
+//   Then My subtotal should be "0" dollars
+
     /**
      * @Given /^I have an empty cart$/
      */
@@ -34,6 +39,48 @@ class FeatureContext extends BehatContext
     {
         $this->cart = new Cart();
     }
+
+// OK
+// Scenario: Add a 10 dollar item to an empty cart
+//   Given I have an empty cart
+//   When I add a "10" dollar item named "shirt"
+//   Then My subtotal should be "10" dollars
+
+    /**
+     * @When /^I add a "([^"]*)" dollar item named "([^"]*)"$/
+     */
+    public function iAddADollarItemNamed($dollars, $product_name)
+    {
+        $this->cart->addNamedItem($product_name,$dollars);
+//         throw new PendingException();
+    }
+
+// OK
+// Scenario: When order is under $100, and all items under 10 lb, then shipping is $5 flat
+//   Given I have an empty cart
+//   When I add a "78" dollar "2" lb item named "dress"
+//   And I add a "20" dollar "1" lb item named "skirt"
+//   Then My subtotal should be "98" dollars
+//   And My total should be "103" dollars
+
+// OK
+// Scenario: When order is $100 or more, and each individual item is under 10 lb, then shipping is free
+//   Given I have an empty cart
+//   When I add a "68" dollar "2" lb item named "dress"
+//   And I add a "20" dollar "1" lb item named "skirt"
+//   And I add a "20" dollar "1" lb item named "skirt"
+//   Then My subtotal should be "108" dollars
+//   And My total should be "108" dollars
+//   And My quantity of products named "skirt" should be "2"
+
+// OK
+// Scenario: Items over 10 lb always cost $20 each to ship
+//   Given I have an empty cart
+//   When I add a "80" dollar "2" lb item named "dress"
+//   And I add a "10" dollar "1" lb item named "tee"
+//   And I add a "50" dollar "100" lb item named "couch"
+//   Then My subtotal should be "140" dollars
+//   And My total should be "160" dollars
 
     /**
      * @Then /^My subtotal should be "([^"]*)" dollars$/
@@ -44,19 +91,12 @@ class FeatureContext extends BehatContext
     }
 
     /**
-     * @When /^I add a "([^"]*)" dollar item named "([^"]*)"$/
-     */
-    public function iAddADollarItemNamed($dollars, $product_name)
-    {
-        throw new PendingException();
-    }
-    
-    /**
      * @When /^I add a "([^"]*)" dollar "([^"]*)" lb item named "([^"]*)"$/
      */
     public function iAddADollarItemWithWeight($dollars, $lb, $product_name)
     {
-        throw new PendingException();
+        $this->cart->addWeighedItem($product_name,$dollars,$lb);
+//         throw new PendingException();
     }
     
     /**
@@ -64,32 +104,55 @@ class FeatureContext extends BehatContext
      */
     public function myTotalShouldBeDollars($total)
     {
-        throw new PendingException();
+        Assert::assertEquals($total, $this->cart->total());
+//         throw new PendingException();
     }
 
-    /**
-     * @Then /^My quantity of products named "([^"]*)" should be "([^"]*)"$/
-     */
-    public function myQuantityOfProductsShouldBe($product_name, $quantity)
-    {
-        throw new PendingException();
-    }
-    
+// OK
+// Scenario: Add an item twice
+//   Given I have a cart with a "5" dollar item named "tee"
+//   When I add a "5" dollar item named "tee"
+//   Then My quantity of products named "tee" should be "2"
 
     /**
      * @Given /^I have a cart with a "([^"]*)" dollar item named "([^"]*)"$/
      */
     public function iHaveACartWithADollarItem($item_cost, $product_name)
     {
-        throw new PendingException();
+        $this->cart = new Cart(); // must have this first to setup a non-empty cart.
+
+        $this->cart->addNamedItem($product_name,$item_cost);
+//         throw new PendingException();
     }
+
+// OK
+// Scenario: Add a 10 dollar item to a cart with a 5 dollar item
+//   Given I have a cart with a "5" dollar item named "tee"
+//   When I add a "10" dollar item named "shirt"
+//   Then My subtotal should be "15" dollars
+
+    /**
+     * @Then /^My quantity of products named "([^"]*)" should be "([^"]*)"$/
+     */
+    public function myQuantityOfProductsShouldBe($product_name, $quantity)
+    {
+        Assert::assertEquals($quantity, $this->cart->quantityByDescription($product_name));
+//         throw new PendingException();
+    }
+    
+// OK
+// Scenario: Apply a 10 percent coupon code to a cart with 10 dollars of items
+//   Given I have a cart with a "10" dollar item named "shirt"
+//   When I apply a "10" percent coupon code
+//   Then My subtotal should be "9" dollars
 
     /**
      * @When /^I apply a "([^"]*)" percent coupon code$/
      */
     public function iApplyAPercentCouponCode($discount)
     {
-        throw new PendingException();
+    	$this->cart->addDiscount($discount);
+//         throw new PendingException();
     }
 
     /**
@@ -97,6 +160,7 @@ class FeatureContext extends BehatContext
      */
     public function myCartShouldHaveItems($item_count)
     {
-        throw new PendingException();
+        throw new PendingException(); // not used.
     }
+
 }
--- src/Hautelook/Cart.php
+++ src/Hautelook/Cart.php
@@ -1,10 +1,116 @@
 <?php
+
+// How to:
+// MacBook-Pro:halo-sample-cart-master wls$ /opt/vagrant/bin/vagrant ssh
+// Last login: Sun Sep 20 23:30:57 2015 from 10.0.2.2
+// vagrant@vagrant-ubuntu-trusty-64:~$ cd /vagrant
+// vagrant@vagrant-ubuntu-trusty-64:/vagrant$ behat
+// •••
+// 	10 scenarios (10 passed)
+// 	44 steps (44 passed)
+// 	0m0.22s
+
 namespace Hautelook;
 
 class Cart
 {
-    public function subtotal()
+// 	private $myCart = array('description'=>'total', 'price'=>0.00, 'weight'=>0, 'quantity'=> 1);
+	private $myCart = array();
+	private $subtotal = 0;
+	private $total    = 0;
+	private $shipping = 0;
+	private $discount = 0; // save this for the whole order.
+	private $weight = 0;
+	private $dbg = 0;
+	
+    public function orig_subtotal()
     {
         return 0;
+    }
+    
+    public function addDiscount($discount) // add a discount to the cart.
+    {
+    	$this->discount += $discount; // += to support an additional discount to the whole order.
+    }
+    
+    public function subtotal() // does not include shipping.
+    {
+    	$this->subtotal = 0;
+    	foreach ($this->myCart as $k=>$v) {
+			$this->subtotal += $v['quantity'] * $this->discountItems($v['price']);
+    	}
+    	return $this->subtotal;
+    }
+
+    public function total() // includes shipping charges.
+    {
+    	$this->total    = 0;
+    	$this->shipping = 0;
+    	$this->weight   = 0;
+    	foreach ($this->myCart as $k=>$v) {
+    		$this->total += $v['quantity'] * $this->discountItems($v['price']);
+    		if ($v['weight'] > 10) {
+    			$this->shipping += 20; // surcharge for a heavy item.
+    		} else {
+    			$this->weight += $v['weight']; // total of non-heavy items.
+    		}
+    	}
+    	if (($this->total < 100) && ($this->weight < 10)) {
+        	$this->shipping += 5; // flat rate shipping in addition to the $20 for over 10 lbs.
+        }
+    	return $this->total + $this->shipping;
+    }
+
+    public function subtotalByDescription($description)
+    {
+    	$this->subtotal = 0;
+    	$this->var_dumper('subtotalByDescription',$this->myCart);
+    	foreach ($this->myCart as $k=>$v) {
+    		if ($v['description'] == $description) {
+				$this->subtotal += $v['quantity'] * $this->discountItems($v['price']);
+				$this->var_dumper('subtotal loop',$this->subtotal);
+    		}
+    	}
+    	$this->var_dumper('subtotal',$this->subtotal);
+    	return $this->subtotal;
+    }
+
+    public function quantityByDescription($description)
+    {
+    	$this->quantity = 0;
+    	$this->var_dumper('quantityByDescription',$this->myCart);
+    	foreach ($this->myCart as $k=>$v) {
+    		if ($v['description'] == $description) {
+				$this->quantity += $v['quantity'];
+				$this->var_dumper('quantity loop',$this->quantity);
+    		}
+    	}
+    	$this->var_dumper('quantity',$this->quantity);
+    	return $this->quantity;
+    }
+
+    public function discountItems($price)
+    {
+		return ($price * ((100 - $this->discount)/100));
+    }
+
+    public function addNamedItem($description,$price,$quantity=1)
+    {
+    	$this->myCart[] = array('description'=>$description, 'price'=>$price, 'weight'=> 0, 'quantity'=> $quantity);
+    	$this->var_dumper('addNamedItem',$this->myCart);
+    }
+
+    public function addWeighedItem($description,$price,$weight)
+    {
+    	$this->myCart[] = array('description'=>$description, 'price'=>$price, 'weight'=> $weight, 'quantity'=> 1);
+    	$this->var_dumper('addWeighedItem',$this->myCart);
+    }
+
+    private function var_dumper($msg,$var)
+    {
+    	if ($this->dbg) {
+			var_dump($msg);
+			var_dump($var);
+    	}
     }
 } 
